1a820e485384433cfda1abe8f922b49500829dce
ymshtshg <tcl0616@gmail.com>
2020-03-16 02:48:26 +0900

    chapter3

--- src/main/scala/Chapter3/Chapter3.scala
+++ src/main/scala/Chapter3/Chapter3.scala
@@ -30,9 +30,30 @@ object Chapter3 {
 //      case _                                     => 101
 //    }
     // -> 3（3番めのケースに入るはず
+
+    // exercise 3.2
+    def tail[A](l: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(_, xs) => xs
+    }
+
+    // exercise 3.3
+    def setHead[A](l: List[A], h: A): List[A] = l match {
+      case Nil => Nil
+      case Cons(_,t) => Cons(h,t)
+    }
+
+    // exercise 3.4
+    def drop[A](l: List[A], n: Int): List[A] =
+      if (n <= 0) l
+      else l match {
+        case Nil => Nil
+        case Cons(_,t) => drop(t, n-1)
+      }
   }
 
   def main: Unit = {
     println("a")
+    val ex2: List[Int] = List(1,2,3,4)
   }
 }
