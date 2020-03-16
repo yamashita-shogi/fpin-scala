a4632a18c44eb00af9b0575eb0988263ab4c25ea
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-03-16 22:28:36 +0900

    chapter3

--- src/main/scala/Chapter3/Chapter3.scala
+++ src/main/scala/Chapter3/Chapter3.scala
@@ -1,9 +1,15 @@
 package Chapter3
 
 object Chapter3 {
+
   sealed trait List[+A] //1
   case object Nil extends List[Nothing] //2
   case class Cons[+A](head: A, tail: List[A]) extends List[A] //3
+
+  def main(args: Array[String]): Unit = {
+    val r = List.init(List(1, 2, 3, 4, 5))
+    println(r)
+  }
 
   object List { //4
     def sum(ints: List[Int]): Int = ints match { //5
@@ -22,38 +28,68 @@ object Chapter3 {
       else Cons(as.head, apply(as.tail: _*))
 
     // exercise 3.1
-//    val x = List(1, 2, 3, 4, 5) match {
-//      case Cons(x, Cons(2, Cons(4, _)))          => x
-//      case Nil                                   => 42
-//      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-//      case Cons(h, t)                            => h + sum(t)
-//      case _                                     => 101
-//    }
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _)))          => x
+      case Nil                                   => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t)                            => h + sum(t)
+      case _                                     => 101
+    }
     // -> 3（3番めのケースに入るはず
 
     // exercise 3.2
     def tail[A](l: List[A]): List[A] = l match {
-      case Nil => Nil
+      case Nil         => Nil
       case Cons(_, xs) => xs
     }
 
     // exercise 3.3
     def setHead[A](l: List[A], h: A): List[A] = l match {
-      case Nil => Nil
-      case Cons(_,t) => Cons(h,t)
+      case Nil        => Nil
+      case Cons(_, t) => Cons(h, t)
     }
 
     // exercise 3.4
     def drop[A](l: List[A], n: Int): List[A] =
       if (n <= 0) l
-      else l match {
-        case Nil => Nil
-        case Cons(_,t) => drop(t, n-1)
-      }
-  }
+      else
+        l match {
+          case Nil        => Nil
+          case Cons(_, t) => drop(t, n - 1)
+        }
 
-  def main: Unit = {
-    println("a")
-    val ex2: List[Int] = List(1,2,3,4)
+    //    // exercise 3.5
+    // 述語とマッチする場合に限りListからその要素までの要素を削除するdropWhileの実装
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+      l match {
+        case Nil        => Nil
+        case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+      }
+
+    // exercise 3.5
+    def append[A](a1: List[A], a2: List[A]): List[A] = {
+      println(a1)
+      a1 match {
+        case Nil        => a2
+        case Cons(h, t) => Cons(h, append(t, a2))
+      }
+    }
+
+    // exercise 3.6
+    def init[A](a1: List[A]): List[A] = {
+      println(a1)
+      a1 match {
+        case Nil        => a1
+        case Cons(h, t) => if (t == Nil) t else Cons(h, init(t))
+      }
+    }
+//    def init[A](a1: List[A]): List[A] = {
+//      println(a1)
+//      a1 match {
+//        case Nil          => a1
+//        case Cons(h, Nil) => Nil
+//        case Cons(h, t)   => Cons(h, init(t))
+//      }
+//    }
   }
 }
