8e7d5c54b2f99b38453ba07256e17de856498955
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-04-20 21:03:09 +0900

    chapter4

--- src/main/scala/Chapter4/Chapter4.scala
+++ src/main/scala/Chapter4/Chapter4.scala
@@ -92,9 +92,57 @@ object Chapter4 {
       }
   }
 
-  sealed trait Either[+E, +A]
+  sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] =
+      this match {
+        case Left(a)  => Left(a)
+        case Right(a) => Right(f(a))
+      }
+
+    def flatMap2[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+      this match {
+        case Left(a)  => Left(a)
+        case Right(a) => f(a)
+      }
+
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+      this match {
+        case Left(_)  => b
+        case Right(a) => Right(a)
+      }
+
+    def map1[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+      this.flatMap2(tt => b.map(bb => f(tt, bb)))
+
+//    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+//      for {
+//        a <- this
+//        b1 <- b
+//      } yield f(a, b1)
+
+//    def map3[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+//      for { a <- this; b1 <- b } yield f(a, b1)
+
+  }
+
   case class Left[+E](value: E) extends Either[E, Nothing]
   case class Right[+A](value: A) extends Either[Nothing, A]
+
+  object Either {
+    def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+      if (xs.isEmpty)
+        Left("mean of empty list!")
+      else
+        Right(xs.sum / xs.length)
+
+    def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+      try Right(x / y)
+      catch { case e: Exception => Left(e) }
+
+    def Try[A](a: => A): Either[Exception, A] =
+      try Right(a)
+      catch { case e: Exception => Left(e) }
+  }
 
   def main(args: Array[String]): Unit = {
     //println("a")
