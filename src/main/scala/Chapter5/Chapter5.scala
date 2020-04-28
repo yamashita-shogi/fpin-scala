1c414ef63892e71c1c5cd4cde866c2a55f03a640
ymshtshg <tcl0616@gmail.com>
2020-04-28 09:55:57 +0900

    chapter5

--- src/main/scala/Chapter5/Chapter5.scala
+++ src/main/scala/Chapter5/Chapter5.scala
@@ -1,6 +1,24 @@
-package Chapter4
+package Chapter5
 
-object Chapter4 {
+object Chapter5 {
+  trait Stream[+A]
+  case object Empty extends Stream[Nothing]
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] ❶
+
+  object Stream {
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = { ❷
+      lazy val head = hd ❸
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+    }
+    def empty[A]: Stream[A] = Empty
+
+    def apply[A](as: A*): Stream[A] =
+      if (as.isEmpty) empty
+      else cons(as.head, apply(as.tail: _*))
+    
+  }
+
   def main(args: Array[String]): Unit = {
     println("a")
   }
