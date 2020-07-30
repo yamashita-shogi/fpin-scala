cb2009fbf54b85cd28e3c56c3523edee85c09e0d
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-07-30 20:26:47 +0900

    chapter11

--- src/main/scala/Chapter11/Chapter11.scala
+++ src/main/scala/Chapter11/Chapter11.scala
@@ -25,17 +25,24 @@ object Chapter11 {
       flatMap(ma)(a => unit(f(a)))
     def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
       flatMap(ma)(a => map(mb)(b => f(a, b)))
+
+    def sequence[A](lma: List[F[A]]): F[List[A]] =
+      lma.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+    def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+      la.foldRight(unit(List[B]()))((a, acc) => map2(f(a), acc)(_ :: _))
   }
 
-  val parMonad = new Monad[Par] {
-    def unit[A](a: => A) = Par.unit(a)
-    def flatMap[A, B](ma: Par[A])(f: A => Par[B]) = Par.flatMap(ma)(f)
-  }
-
-  def parserMonad[P[+ _]](p: Parsers[P]) = new Monad[P] {
-    def unit[A](a: => A) = p.succeed(a)
-    def flatMap[A, B](ma: P[A])(f: A => P[B]) = p.flatMap(ma)(f)
-  }
+//  ParとParserはやってないしスルー
+//  val parMonad = new Monad[Par] {
+//    def unit[A](a: => A) = Par.unit(a)
+//    def flatMap[A, B](ma: Par[A])(f: A => Par[B]) = Par.flatMap(ma)(f)
+//  }
+//
+//  def parserMonad[P[+ _]](p: Parsers[P]) = new Monad[P] {
+//    def unit[A](a: => A) = p.succeed(a)
+//    def flatMap[A, B](ma: P[A])(f: A => P[B]) = p.flatMap(ma)(f)
+//  }
 
   val optionMonad = new Monad[Option] {
     def unit[A](a: => A) = Some(a)
@@ -50,12 +57,6 @@ object Chapter11 {
   val listMonad = new Monad[List] {
     def unit[A](a: => A) = List(a)
     def flatMap[A, B](ma: List[A])(f: A => List[B]) = ma flatMap f
-  }
-
-  val satreamMonad = new Monad[Stream] {
-    override def unit[A](a: => A): Stream[A] = a[A]
-    def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] =
-      ma flatMap f
   }
 
   def main(args: Array[String]): Unit = {
