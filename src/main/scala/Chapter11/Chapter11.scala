a66a9a6f22f758762cf071adebc7f9f9578e96db
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-08-25 20:13:16 +0900

    chapter11

--- src/main/scala/Chapter11/Chapter11.scala
+++ src/main/scala/Chapter11/Chapter11.scala
@@ -154,14 +154,18 @@ object Chapter11 {
       ._1
       .reverse
 
-  def _zipWithIndex[A](as: List[A]): State[Int, List[(Int, A)]] =
+  def _zipWithIndex[A](as: List[A]): List[(Int, A)] =
     as.foldLeft(F.unit(List[(Int, A)]()))((acc, a) =>
-      for {
-        xs <- acc
-        n <- getState
-        _ <- setState(n + 1)
-      } yield (n, a) :: xs
-    )
+        acc
+          .flatMap(xs => {
+            getState.flatMap(n =>
+              {
+                setState(n + 1)
+              }.map((n, a) :: xs)
+            )
+          })
+      )
+      .run(0)
 
   case class Reader[R, A](run: R => A)
   object Reader {
@@ -211,10 +215,30 @@ object Chapter11 {
 //    println(map2.run(5))
 //    println(sequence.run(5))
 
-    val aa = a.run(0)
-    println(aa._1.map(_ => getState))
-
-    println(zipWithIndex(List(10, 20, 30)))
+    //    println(zipWithIndex(List(10, 20, 30)))
 //    println(_zipWithIndex(List(10, 20, 30)))
+
+    // exercise 11.20
+    val f1 = (i: Int) => s"${i * 10} * 10"
+    val f2 = (i: Int) => s"${i * 20} * 20"
+    val readerMonad: Monad[
+      ({
+        type f[x] = Reader[Int, x]
+      })#f
+    ] = Reader.readerMonad
+    // flatMapは関数を実行した結果から再度 Readerを定義する
+    val fMapReader = Reader(f1)
+    val flatMapResult =
+      readerMonad.flatMap(fMapReader)(s => Reader(r => s"$s * ${r.toString}"))
+    println(s"result flatMap: ${flatMapResult.run(2)}")
+    val sequenceReader = List(Reader(f1), Reader(f2))
+    val sequenceResult = readerMonad.sequence(sequenceReader)
+    println(s"result sequence: ${sequenceResult.run(2)}")
+    val f3 = (i: Int) => Reader[Int, String](ii => s"${i + ii} *")
+    val joinReader: Reader[Int, Reader[Int, String]] = Reader(f3)
+    val joinResult = readerMonad.join(joinReader)
+    println(s"result join: ${joinResult.run(2)}")
+    val replicateMResult = readerMonad.replicateM(5, Reader(f1))
+    println(s"result replicateM: ${replicateMResult.run(2)}")
   }
 }
