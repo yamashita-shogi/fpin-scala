b81729d7ba2701472f7973e4e7ab889a47f26598
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-09-08 19:36:34 +0900

    chapter12

--- src/main/scala/Chapter12/Chapter12.scala
+++ src/main/scala/Chapter12/Chapter12.scala
@@ -1,6 +1,6 @@
 package Chapter12
 
-import Chapter11.Chapter11.Functor
+import Chapter11.Chapter11.{Functor, Monad}
 
 object Chapter12 {
   trait Applicative[F[_]] extends Functor[F] {
@@ -26,6 +26,7 @@ object Chapter12 {
       map2(fa, fb)((_, _))
   }
 
+  // exercise 12.2
   trait _Applicative[F[_]] extends Functor[F] {
     // `map2` is implemented by first currying `f` so we get a function
     // of type `A => B => C`. This is a function that takes `A` and returns
@@ -61,6 +62,48 @@ object Chapter12 {
       apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
   }
 
+  val streamApplicative = new Applicative[Stream] {
+//  val streamApplicative = new _Applicative[Stream] {
+    def unit[A](a: => A): Stream[A] =
+      Stream.continually(a)
+
+    override def map2[A, B, C](a: Stream[A], b: Stream[B])(
+        f: (A, B) => C): Stream[C] =
+      a zip b map f.tupled
+
+    //    def sequence[A](lma: List[F[A]]): F[List[A]] =
+    override def sequence[A](a: List[Stream[A]]): Stream[List[A]] =
+      a.foldRight(unit(List[A]()))((a, acc) => map2(a, acc)(_ :: _))
+  }
+
+  def eitherMonad[E] = new Monad[({ type f[x] = Either[E, x] })#f] {
+    def unit[A](a: => A) = Right(a)
+
+    def flatMap[A, B](e: Either[E, A])(f: A => Either[E, B]): Either[E, B] =
+      e match {
+        case Left(a)  => Left(a)
+        case Right(a) => f(a)
+      }
+  }
+
+  sealed trait Validation[+E, +A]
+  case class Failure[E](head: E, tail: Vector[E] = Vector())
+  case class Success[A](a: A) extends Validation[Nothing, A]
+
+  def validationApplicative[E] =
+    new Applicative[({ type f[x] = Validation[E, x] })#f] {
+      def unit[A](a: => A) = Success(a)
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(
+          f: (A, B) => C) =
+        (fa, fb) match {
+          case (Success(a), Success(b)) => Success(f(a, b))
+          case (Failure(h1, t1), Failure(h2, t2)) =>
+            Failure(h1, t1 ++ Vector(h2) ++ t2)
+          case (e @ Failure(_, _), _) => e
+          case (_, e @ Failure(_, _)) => e
+        }
+    }
+
 //  // Monad[F]をApplicative[F]の部分型（subtype）にすることが可能です
 //  trait Monad[F[_]] extends Applicative[F] {
 //    def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] =
@@ -78,6 +121,15 @@ object Chapter12 {
 //      flatMap(fa)(a => map(fb)(b => f(a, b)))
 //  }
 
-  def main(args: Array[String]): Unit =
-    println("a")
+  def main(args: Array[String]): Unit = {
+
+    // Streamのリストから、Streamをリストにまとめたものにする
+    println(
+      streamApplicative
+        .sequence(List(Stream(1, 2), Stream(3, 4)))
+        .foreach(println))
+//    println(_streamApplicative.sequence(List(Stream(1, 2, 3, 4))))
+
+  }
+
 }
