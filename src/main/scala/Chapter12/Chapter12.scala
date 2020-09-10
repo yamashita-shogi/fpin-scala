50dd5f67af8c458d310834ce3d54909cc6d16ac0
yamashita-shogo <yamashita_shogo@cyberagent.co.jp>
2020-09-10 18:38:58 +0900

    chapter12

--- src/main/scala/Chapter12/Chapter12.scala
+++ src/main/scala/Chapter12/Chapter12.scala
@@ -88,6 +88,7 @@ object Chapter12 {
 
   sealed trait Validation[+E, +A]
   case class Failure[E](head: E, tail: Vector[E] = Vector())
+      extends Validation[E, Nothing]
   case class Success[A](a: A) extends Validation[Nothing, A]
 
   def validationApplicative[E] =
